05e00a20036a6c461bb1d1f04da11f0004c9dcdc
kiadzaky <kkhiahebat@gmail.com>
2021-11-11 11:37:26 +0700

    SPA
    membuat SPA

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Belajar React - API menggunakan JSON SERVER
 1. npx json-server --watch db.json --port 6969
 2. npm start
 3. CRUD berjalan
+4. membuat SPA
--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import { BrowserRouter as BR, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as BR,
+	Routes,
+	Route,
+	HashRouter,
+	NavLink,
+} from 'react-router-dom';
 import ProductList from './components/ProductList';
 import AddProduct from './components/AddProduct';
 import EditProduct from './components/EditProduct';
 function App() {
 	return (
-		<div className="container">
-			<BR>
-				<Routes>
-					<Route exact path="/" element={<ProductList />}></Route>
-					<Route path="/addproduct" element={<AddProduct />}></Route>
-					<Route path="/editproduct/:id" element={<EditProduct />}></Route>
-				</Routes>
-			</BR>
-		</div>
+		// MEMBUAT ROUTE
+		// <div className="container">
+		// 	<BR>
+		// 		<Routes>
+		// 			<Route exact path="/" element={<ProductList />}></Route>
+		// 			<Route path="/addproduct" element={<AddProduct />}></Route>
+		// 			<Route path="/editproduct/:id" element={<EditProduct />}></Route>
+		// 		</Routes>
+		// 	</BR>
+		// </div>
+		//MEMBUAT SPA
+		<HashRouter>
+			<div>
+				<h1 className="title">Single Page Application</h1>
+				<ul>
+					<li>
+						<NavLink to="/">List Product</NavLink>
+					</li>
+					<li>
+						<NavLink to="/addproduct">Tambah Product</NavLink>
+					</li>
+				</ul>
+				<div className="content">
+					<Routes>
+						<Route exact path="/" element={<ProductList />}></Route>
+						<Route path="/addproduct" element={<AddProduct />}></Route>
+						<Route path="/editproduct/:id" element={<EditProduct />}></Route>
+					</Routes>
+				</div>
+			</div>
+		</HashRouter>
 	);
 }
 
